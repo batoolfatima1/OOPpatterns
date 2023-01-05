b6e7e46974defb390c36142b6c0f812a1c769d39
batoolfatima1 <batool12547@gmail.com>
2023-01-05 23:50:49 +0500

    latest

---
+++ SolidRhombous.java
@@ -0,0 +1,25 @@
+//     xxxxx
+//    xxxxx
+//   xxxxx
+//  xxxxx
+// xxxxx
+
+public class SolidRhombous
+{
+public static void main(String[] args) {
+    int n=8 ; //rows , columns
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=n-i; j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1; j<=n ;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+}
