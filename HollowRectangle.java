748b1e4a1b795bc1170d42b21c06131e3e704608
batoolfatima1 <batool12547@gmail.com>
2023-01-05 22:32:24 +0500

    first commit

---
+++ HollowRectangle.java
@@ -0,0 +1,31 @@
+// * * * * * *
+// *         *
+// *         * 
+// *         *
+// *         *
+// *         *
+// *         *
+// * * * * * *
+
+class HollowRectangle
+{
+    public static void main(String[] args) {
+        int n = 6; // col
+        int m = 8; // row
+        for(int i=1 ; i<=m;  i++)
+        {
+            for(int j=1 ; j<=n; j++)
+            {
+                if(i== 1 || i== m || j==1 || j==n)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
