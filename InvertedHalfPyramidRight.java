dd2b9f4f90c34bdfea54e880a2fb2c8ca7f36617
batoolfatima1 <batool12547@gmail.com>
2023-01-05 22:57:58 +0500

    latest

---
+++ InvertedHalfPyramidRight.java
@@ -0,0 +1,26 @@
+
+// * * * * *
+//   * * * *
+//     * * * 
+//       * * 
+//         * 
+
+public class InvertedHalfPyramidRight {
+    public static void main(String[] args) {
+        int n=5;
+        for( int i=n; i>=1 ; i--)
+        {
+            // for space
+            for(int j =1 ; j<= n-i ; j++)
+            {
+                System.out.print("   ");
+            }
+            for(int j =1; j<= i ;j++)
+            {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
