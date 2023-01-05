f44211d72f8a13486442239a82dbb2cbb3c2fe03
batoolfatima1 <batool12547@gmail.com>
2023-01-05 23:09:13 +0500

    latest

---
+++ Diamond.java
@@ -0,0 +1,38 @@
+
+
+public class Diamond {
+   // pyramid + inverted pyramid 
+   public static void main(String[] args) {
+    int n=5;
+    // for pyramid
+    for(int i=1 ; i<=n ; i++)
+     {
+        //for space 
+        for(int j=1; j<= n-i ; j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1 ; j<=(2*i)-1 ;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+        
+     }
+     // inveretd pyramid
+     for(int i=n ; i>=1 ; i--)
+     {
+        //for space 
+        for(int j=1; j<= n-i ; j++)
+        {
+            System.out.print(" ");
+        }
+        for(int j=1 ; j<=(2*i)-1 ;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+        
+     }
+   }
+}
