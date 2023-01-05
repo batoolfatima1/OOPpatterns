41ea6e5e8fa1a35fe190da9e5cc6e28a898b8178
batoolfatima1 <batool12547@gmail.com>
2023-01-05 23:34:35 +0500

    latest

---
+++ Butterfly.java
@@ -0,0 +1,61 @@
+//   *      *
+//   **    **
+//   ***  ***
+//   ********
+//   ********
+//   ***  ***  
+//   **    **
+//   *      *
+
+public class Butterfly {
+    //upper + lower
+
+    public static void main(String[] args) {
+        int n=4;
+        // upper
+        //   *      *
+        //   **    **
+        //   ***  ***
+        //   ********
+        for(int i=1; i<=n ;i++)
+        {
+            for(int j =1 ; j<=i ; j++)
+            {
+                System.out.print("*");
+            }
+
+            for(int k=1; k<= 2*(n-i); k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j =1 ; j<=i ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower
+        //   ********
+        //   ***  ***  
+        //   **    **
+        //   *      *
+        for(int i=n; i>=1 ;i--) 
+        {
+            for(int j =1 ; j<=i ; j++)
+            {
+                System.out.print("*");
+            }
+
+            for(int k=1; k<= 2*(n-i); k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j =1 ; j<=i ; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
